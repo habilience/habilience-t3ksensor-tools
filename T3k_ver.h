8e6b8c90b6d2a6bab2fecf00f01ffd0f58381920
windnsoul <windnsoul@gmail.com>
2014-03-24 01:53:03 +0000

    2.8c.5 Test

--- T3k_ver.h
+++ T3k_ver.h
@@ -1,8 +1,8 @@
 ﻿#ifndef T3KS_VERSION
 #define T3KS_VERSION
 
-#define T3000_FILEVERSION 		2,8c,2,0
-#define T3000_VERSION			"2.8c.2"
+#define T3000_FILEVERSION 		2,8c,5,0
+#define T3000_VERSION			"2.8c.5"
 
 
 #define MODEL_T3000			0x01
