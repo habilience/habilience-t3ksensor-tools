ac04fdb066154621fbba33ea8455ba0f05a21e0b
windnsoul <windnsoul@gmail.com>
2014-09-23 04:38:04 +0000

    ver 2.8e release

--- build_win.bat
+++ build_win.bat
@@ -1,6 +1,21 @@
 rem 환경에 맞추어 설정
 set PATH=%PATH%;C:\Qt\Qt5.2.0\Tools\mingw48_32\bin
 set QMAKE=C:\Qt\qt-everywhere-opensource-src-5.2.0\qtbase\bin
+set FWPATH=E:\Project\TouchSensor
+
+set chk_copy = n
+
+set /p chk_copy=Copy Firmware? (Y/N):
+
+if %chk_copy%==y goto copy_firmware
+if %chk_copy%==Y goto copy_firmware
+goto skip_copy
+
+:copy_firmware
+
+copy %FWPATH%\T3kApp_Release\Binary\x.x.fwb T3kCfgFE\resources\firmupBinary.fwb /Y
+
+:skip_copy
 
 mkdir release
 
